ff1d9ee16ded5ea68e56420b035ef2fc3267c436
BTELNYY <btelnyy@gmail.com>
2024-09-03 18:41:13 -0600

    unfinished array stuff

---
+++ UnityConvar/BuiltIn/ConsoleVariableArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityConvar.Attributes;
+
+namespace UnityConvar.BuiltIn
+{
+    [ConsoleVariableType]
+    public class ConsoleVariableArray<T> : ConsoleVariable<IEnumerable<T>>, IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            return Value.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Value.GetEnumerator();
+        }
+    }
+}
---
+++ UnityConvar/BuiltIn/DoubleConsoleVariable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityConvar.Attributes;
+
+namespace UnityConvar.BuiltIn
+{
+    [ConsoleVariableType]
+    public class DoubleConsoleVariable : ConsoleVariable<double>
+    {
+        public override double Parse(string value)
+        {
+            if(!double.TryParse(value, out double result))
+            {
+                HandleParseError(value);
+            }
+            return result;
+        }
+    }
+}
